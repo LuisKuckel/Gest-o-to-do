b1dfdd69cd948c332438279971adec75a46dba29
amanda silveira <amanda.silveira0w@gmail.com>
2024-11-05 18:10:08 -0300

    Front+Back Organization

---
+++ Back/Calenderdeaulas/src/main/java/Seminario/web/Calenderdeaulas/CalenderdeaulasApplication.java
@@ -0,0 +1,13 @@
+package Seminario.web.Calenderdeaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderdeaulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderdeaulasApplication.class, args);
+	}
+
+}
